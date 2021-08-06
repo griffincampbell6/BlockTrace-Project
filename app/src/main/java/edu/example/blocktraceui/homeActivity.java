7b8aa45ad5a4746cd23b210f407aa02bf37420ae
rankinsm <rankinsm@wit.edu>
2021-08-06 17:46:06 -0400

    Splash Screen

---
+++ app/src/main/java/edu/example/blocktraceui/homeActivity.java
@@ -0,0 +1,14 @@
+package edu.example.blocktraceui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class homeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_screen);
+    }
+}
