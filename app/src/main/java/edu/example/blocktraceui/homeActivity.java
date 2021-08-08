581dd1ee992276db7588998d5d68a06ad71edfdf
rankinsm <rankinsm@wit.edu>
2021-08-08 13:16:16 -0400

    Create Contact Popup

--- app/src/main/java/edu/example/blocktraceui/homeActivity.java
+++ app/src/main/java/edu/example/blocktraceui/homeActivity.java
@@ -1,13 +1,19 @@
 package edu.example.blocktraceui;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class homeActivity extends AppCompatActivity {
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+    private EditText contactName;
+    private Button btn_createCancel, btn_createSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +37,7 @@ public class homeActivity extends AppCompatActivity {
         Button createContract = (Button) findViewById(R.id.btn_createContract);
         editIcon.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                switchToCreateActivity();
+                createContactDialog();
             }
         });
 
@@ -47,8 +53,29 @@ public class homeActivity extends AppCompatActivity {
         startActivity(switchActivityIntent);
     }
 
-    private void switchToCreateActivity() {
-        Intent switchActivityIntent = new Intent(this, EditProfile.class);
-        startActivity(switchActivityIntent);
+    public void createContactDialog(){
+        dialogBuilder = new AlertDialog.Builder(this);
+        final View createContact = getLayoutInflater().inflate(R.layout.create_contact, null);
+
+        btn_createCancel = (Button) createContact.findViewById(R.id.btn_createCancel);
+        btn_createSave = (Button) createContact.findViewById(R.id.btn_createSave);
+
+        dialogBuilder.setView(createContact);
+        dialog = dialogBuilder.create();
+        dialog.show();
+
+        btn_createCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        btn_createSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
