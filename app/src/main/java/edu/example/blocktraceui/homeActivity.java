5745f1b0f594676521728105453b0afefcd9c477
rankinsm <rankinsm@wit.edu>
2021-08-08 20:55:22 -0400

    Home Screen redesigned - Removed toolbar at the bottom and home button, Implemented report status feature, Added logout option in setttings

--- app/src/main/java/edu/example/blocktraceui/homeActivity.java
+++ app/src/main/java/edu/example/blocktraceui/homeActivity.java
@@ -3,14 +3,20 @@ package edu.example.blocktraceui;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.PopupMenu;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class homeActivity extends AppCompatActivity {
-    private AlertDialog.Builder dialogBuilder;
+public class homeActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+    private AlertDialog.Builder dialogBuilderCreate, dialogBuilderReport;
     private AlertDialog dialog;
     private EditText contactName;
     private Button btn_createCancel, btn_createSave;
@@ -19,6 +25,13 @@ public class homeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
+
+        Button settingIcon = (Button) findViewById(R.id.btn_settings);
+        settingIcon.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                settingsPopup(view);
+            }
+        });
 
         Button contactIcon = (Button) findViewById(R.id.btn_contacts);
         contactIcon.setOnClickListener(new View.OnClickListener() {
@@ -34,14 +47,69 @@ public class homeActivity extends AppCompatActivity {
             }
         });
 
-        Button createContract = (Button) findViewById(R.id.btn_createContract);
-        editIcon.setOnClickListener(new View.OnClickListener() {
+        Button createContact = (Button) findViewById(R.id.btn_createContact);
+        createContact.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 createContactDialog();
             }
         });
 
+        Button btnReport = (Button) findViewById(R.id.btnReport);
+        btnReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogBuilderReport = new AlertDialog.Builder(homeActivity.this);
+                View reportView = getLayoutInflater().inflate(R.layout.report_spinner, null);
+                dialogBuilderReport.setCancelable(true);
+                dialogBuilderReport.setTitle("Report Status");
+                dialogBuilderReport.setMessage("Please select your latest test result status.");
+                Spinner reportSpin = (Spinner) reportView.findViewById(R.id.spn_Report);
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(homeActivity.this,
+                        android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.results));
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+                reportSpin.setAdapter(adapter);
+                dialogBuilderReport.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if(!reportSpin.getSelectedItem().toString().equalsIgnoreCase("Please select a test result...")){
+                            Toast.makeText(homeActivity.this,
+                                    reportSpin.getSelectedItem().toString() + "Result Reported", Toast.LENGTH_SHORT).show();
+                            //GET TEST RESULTS
+                            dialogInterface.dismiss();
+                        }
+                    }
+                });
+                dialogBuilderReport.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                dialogBuilderReport.setView(reportView);
+                dialog = dialogBuilderReport.create();
+                dialog.show();
+            }
+        });
+
     }
+
+    private void settingsPopup(View v){
+        PopupMenu popup = new PopupMenu(this, v);
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.settings_menu);
+        popup.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.logout:
+                setContentView(R.layout.activity_main);
+                return true;
+        }
+        return false;
+    }
+
 
     private void switchToContactsActivity() {
         Intent switchActivityIntent = new Intent(this, Contacts.class);
@@ -54,14 +122,14 @@ public class homeActivity extends AppCompatActivity {
     }
 
     public void createContactDialog(){
-        dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilderCreate = new AlertDialog.Builder(this);
         final View createContact = getLayoutInflater().inflate(R.layout.create_contact, null);
 
         btn_createCancel = (Button) createContact.findViewById(R.id.btn_createCancel);
         btn_createSave = (Button) createContact.findViewById(R.id.btn_createSave);
 
-        dialogBuilder.setView(createContact);
-        dialog = dialogBuilder.create();
+        dialogBuilderCreate.setView(createContact);
+        dialog = dialogBuilderCreate.create();
         dialog.show();
 
         btn_createCancel.setOnClickListener(new View.OnClickListener() {
@@ -78,4 +146,5 @@ public class homeActivity extends AppCompatActivity {
             }
         });
     }
+
 }
